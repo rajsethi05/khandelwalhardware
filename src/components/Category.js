fa4be18d0568a55c8f0d37ca2100874d9d2e80ff
rajsethi05 <raj.sethi05@gmail.com>
2024-08-06 18:54:23 -0400

    added working Download button

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,21 +2,29 @@ import React from "react";
 import "./Category.css";
 import data from "../resources/categories.json"
 import { Link } from "react-router-dom";
+// import "./Video.css";
 
-export default function Categories(){
+export default function Categories() {
 
-    return(
+    return (
         <div>
             <img className="heading" src="icons/hindware.png" alt="hindware"></img>
-            <div className="line" key=""/>
+            <div className="line" key="" />
             <div>
-                {data.map((item,idx) => (
-                    <Link className="category" to='/products' state={{prod_name:item.page}}>
-                    <img src={item.src} alt={idx} key={idx} className="img" />
-                    <h3 className="txt">{item.title}</h3>
-                    <p className="txt">{item.desc}</p>
+                {data.map((item, idx) => (
+                    <Link className="category" to='/products' state={{ prod_name: item.page }}>
+                        <img src={item.src} alt={idx} key={idx} className="img" />
+                        <h3 className="txt">{item.title}</h3>
+                        <p className="txt">{item.desc}</p>
                     </Link>
                 ))}
+            </div>
+            <div className="btn-div">
+                <a href='documents\\Full_Catalogue.pdf' download="Full_Catalogue.pdf">
+                    <button className="video-button download-btn">Download Full Catalogue
+                        <img src="icons\cloud_down.svg" alt="play" className="download-icon"></img>
+                    </button>
+                </a>
             </div>
         </div>
     )
--- src/components/Video.js
+++ src/components/Video.js
@@ -64,7 +64,6 @@ function Video(){
         </button>
         </div>
         {/* <Mod/> */}
-
         
         </>
     )
