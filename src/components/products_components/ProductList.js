7647d1bbffb2746042a68c49b2bbbc0bcfc67f1e
rajsethi05 <raj.sethi05@gmail.com>
2024-08-16 14:55:21 -0400

    1. Added more prod link 2. adjusted download buttons

--- src/components/products_components/ProductList.js
+++ src/components/products_components/ProductList.js
@@ -4,13 +4,11 @@ import "./ProductList.css";
 const ProductList = ({ prodname }) => {
     const products = prodname;
 
-
     return (
         <>
-            {/* {(prodname) => (console.log("pp:", prodname[0]))} */}
             <div className="prods-container">
                 {
-                    products[0].products.map((prod, idx) => (
+                    products.products_list.map((prod, idx) => (
                         < div className="prod_new" style={{ animationDelay: idx + "s" }}>
                             <img src={prod.pic} className="prod-image_new"></img>
                             <p className="prod-title_new">{prod.name}
